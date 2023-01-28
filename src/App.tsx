cfc5d60761036895e555fbb65d6f441cbcefa68f
José Carlos <etijcvivi@gmail.com>
2023-01-27 23:21:33 -0300

    refactor: adicionando novo componente e removendo componetes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { Button, CardImg } from "react-bootstrap";
+// import {} from "react-bootstrap";
 import Cabecalho from "./components/Header";
+import Home from "./components/Home";
 function App() {
   return (
     <div className="App">
       <Cabecalho />
-      <Button variant="primary">Primary</Button>{" "}
+      <Home />
+      {/* <Button variant="primary">Primary</Button>{" "}
       <Button className="btn-outro" variant="primary">
         Teste
-      </Button>{" "}
+      </Button>{" "} */}
     </div>
   );
 }
