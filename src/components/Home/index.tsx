ecde0de4546e44a1514d47f68247fd2e8521c6be
José Carlos <etijcvivi@gmail.com>
2023-01-29 22:12:03 -0300

    refactor: adicionando novoc componentes

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,19 +1,19 @@
-import { Image, Container } from "react-bootstrap";
+import { Image, Container, Badge } from "react-bootstrap";
 import Foto from "./assets/minhafoto.jpeg";
+import Background from "./assets/background1.jpeg";
 
 const Home = () => {
   return (
-    <div className="imgBackground">
-      <Container
-        fluid
-        // style={{ backgroundColor: "lightgray" }}
-        className="d-flex justify-content-center"
-      >
-        <Image
-          className="header__container__image py-5 w-25 h-25"
-          roundedCircle
-          src={Foto}
-        />
+    <div
+      style={{ backgroundImage: `url(${Background})` }}
+      className="backgroundImage mx-auto w-100"
+    >
+      <Container className="d-flex justify-content-evenly align-items-center">
+        <h1 className="fs-1 text-white d-flex flex-column">
+          José Carlos Rodrigues Filho
+          <Badge bg="secondary">Frontend Developer</Badge>
+        </h1>
+        <Image roundedCircle className="py-5 w-25 h-25" src={Foto} />
       </Container>
     </div>
   );
