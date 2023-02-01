a4538772c665c8dc6b69b778278e66caa74b5d61
José Carlos <etijcvivi@gmail.com>
2023-01-31 23:32:53 -0300

    feat: Adicionando novo componente

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Image, Container, Badge } from "react-bootstrap";
+import { Image, Container, Badge, Card } from "react-bootstrap";
 import Foto from "./assets/minhafoto.jpeg";
 import Background from "./assets/background1.jpeg";
 
@@ -11,10 +11,37 @@ const Home = () => {
       <Container className="d-flex justify-content-evenly align-items-center">
         <h1 className="fs-1 text-white d-flex flex-column">
           José Carlos Rodrigues Filho
-          <Badge bg="secondary">Frontend Developer</Badge>
+          <Badge bg="secondary" className="boxCard mt-4">
+            Frontend Developer
+          </Badge>
         </h1>
         <Image roundedCircle className="py-5 w-25 h-25" src={Foto} />
       </Container>
+      <div className="d-flex justify-content-center">
+        <div className="boxCard w-50 p-3 bg-secondary rounded-3">
+          <span className="fs-3 text-white text-center">
+            Desenvolvedor frontend com contribuições de projetos permanentes no
+            GitHub, construção de single page application, landing pages,
+            websites modernos com metodologia ágil.
+          </span>
+        </div>
+      </div>
+      {/* <div className="d-flex justify-content-around">
+        <div className="w-25">
+          <h2 className="fs-3 text-white text-center">
+            Desenvolvedor frontend com contribuições de projetos permanentes no
+            GitHub, construção de single page application, landing pages,
+            websites modernos com metodologia ágil.
+          </h2>
+        </div>
+        <Card className="w-25  bg-secondary">
+          <Card.Body className="fs-3 text-white text-center">
+            Desenvolvedor frontend com contribuições de projetos permanentes no
+            GitHub, construção de single page application, landing pages,
+            websites modernos com metodologia ágil.
+          </Card.Body>
+        </Card>
+      </div> */}
     </div>
   );
 };
