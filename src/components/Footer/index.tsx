e08da3520167301c54df6be056d61c552a1e06db
José Carlos <etijcvivi@gmail.com>
2023-02-02 23:20:13 -0300

    feat: criação do componente Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer>
+      <h3>
+        &copy; Copyright{" "}
+        <a href="https://github.com/JoseCarlos-Filho" target="_blank">
+          José Carlos Rodrigues Filho
+        </a>
+      </h3>
+    </footer>
+  );
+};
+
+export default Footer;
