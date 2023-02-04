3df6cb883812ac969a44ce440d656fc0bacb3267
José Carlos <etijcvivi@gmail.com>
2023-02-03 22:42:57 -0300

    feat: adicionando novas rotas em App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Contato from "./Views/Contato";
 import Home from "./Views/Home";
+import Projetos from "./Views/Projetos";
+import Sobre from "./Views/Sobre";
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/projetos" element={<Projetos />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/contato" element={<Contato />} />
         </Routes>
       </Router>
     </div>
