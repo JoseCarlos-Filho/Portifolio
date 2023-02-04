f6433d2c3c426ddec04a057edb2aa242eea19975
José Carlos <etijcvivi@gmail.com>
2023-02-03 22:46:00 -0300

    refactor: adicionando estilos ao copyright

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer>
-      <h3>
+      <h3 className="copy  ps-5">
         &copy; Copyright{" "}
         <a href="https://github.com/JoseCarlos-Filho" target="_blank">
           José Carlos Rodrigues Filho
