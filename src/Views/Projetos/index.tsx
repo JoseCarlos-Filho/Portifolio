8194a19af2dad938197353adce93fa9768f4f8e3
José Carlos <etijcvivi@gmail.com>
2023-02-04 23:02:47 -0300

    feat: page Projetos grid com cards e cabeçalho da page

--- src/Views/Projetos/index.tsx
+++ src/Views/Projetos/index.tsx
@@ -1,7 +1,23 @@
+import Cabecalho from "../../components/Header";
+import { Container, Row, Col } from "react-bootstrap";
+
 const Projetos = () => {
   return (
     <div>
+      <Cabecalho />
       <h1>Página Projetos</h1>
+      <Container>
+        <Row>
+          <Col sm={4}>Card 1</Col>
+          <Col sm={4}>Card 2</Col>
+          <Col sm={4}>Card 3</Col>
+        </Row>
+        <Row>
+          <Col sm>Card 4</Col>
+          <Col sm>Card 5</Col>
+          <Col sm>Card 6</Col>
+        </Row>
+      </Container>
     </div>
   );
 };
