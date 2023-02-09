e39b49ddedd2295dffed588dc25e4e32c95a65f6
José Carlos <etijcvivi@gmail.com>
2023-02-08 23:08:34 -0300

    fix: aterando extenção tsx para ts

--- src/services/api.tsx
+++
@@ -1,7 +0,0 @@
-import { Octokit } from "@octokit/rest";
-
-const apiGit = new Octokit({
-  auth: "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",
-});
-
-export default apiGit;
