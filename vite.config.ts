75e367e1b193f3f207afea3d75ead118234fbb72
José Carlos <etijcvivi@gmail.com>
2023-02-09 23:42:28 -0300

    fix: correção de configuração do vite para node-fetch

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: "globalThis",
-      },
+  resolve: {
+    alias: {
+      "node-fetch": "isomorphic-fetch",
     },
   },
 });
