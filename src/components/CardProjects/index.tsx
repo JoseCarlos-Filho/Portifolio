f0bde4b630d5f24d84c3d42962f28c4d3f987463
José Carlos <etijcvivi@gmail.com>
2023-02-15 22:37:00 -0300

    feat: componente CardProjetos

---
+++ src/components/CardProjects/index.tsx
@@ -0,0 +1,16 @@
+import { Card, Button } from "react-bootstrap";
+import { IPropsCardRepo } from "./types";
+
+const CardProjetos = ({ id, name, description }: IPropsCardRepo) => {
+  return (
+    <Card style={{ width: "18rem" }} className="mb-5" id={String(id)}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+        <Button variant="primary">Deploy</Button>
+      </Card.Body>
+    </Card>
+  );
+};
+export default CardProjetos;
